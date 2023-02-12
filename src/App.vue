118e5b735f690257edd377347a7434697277578e
Jps709 <jps7095@psu.edu>
2023-02-12 13:32:37 -0500

    Updoot

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,60 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+ 
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+button {
+  cursor:pointer;
+  background-color:#4681f4;
+  color:white;
+  border-radius:10px;
+  padding:6px;
+  font-size:25px;
+  font-weight:bold;
+  text-align:center;
+  width:14%;
+  
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.outline {
+ justify-content:center;
+ display:flex;
+ min-height:400px;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.lecard{
+ text-align: center;
+ border: solid grey 6px;
+ max-width:400px;
+ border-radius: 20px;
+ margin-bottom: 10px;
+ font-size:20px;
+ background-color:white;
+}
+img{
+  width: 350px;
+}
+
+@media screen and (min-width:501px) and (max-width:800px) {
+
+}
+@media screen and (max-width:500px) {
+  .lecard{
+    font-size: 5vw
+  }
+}
+.buttons {
+  margin: 16px auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly; 
+}
+button:hover{
+  background-color:red;
+}
+button:focus{
+ background-color:red;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,24 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div class="buttons">
+  
+  <button class="copyCat">Copy</button>
+  <button id="del">Delete</button>
+  <button class="btnDet">Toggle Details</button> 
+  <button class="btnCT">Change Title</button>
+  <button  class="btnBG">Change Background Color</button>
+  
   </div>
+<div class="outline">
+  <div class="lecard">
+   <h2>Some Nerd</h2>
+ 
+  <img src="https://i.kym-cdn.com/news_feeds/icons/mobile/000/037/359/05b.jpg" alt="Obi-Wan headshot" >
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+    <p class="details">Hello there <br> Lightsaber go <i>brrrr</i></p>
+    </div>
+</div>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
+<style >
 </style>
